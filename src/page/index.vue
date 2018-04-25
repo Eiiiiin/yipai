9ea6681e5dc39fb2743427d0c4b443fed814a325
Eiiiiin <34846975+Eiiiiin@users.noreply.github.com>
2018-04-25 17:38:53 +0800

    0.3

--- src/page/index.vue
+++ src/page/index.vue
@@ -8,7 +8,10 @@
         <h1>新一代响应式网站设计工具</h1>
         <p>简单 专业 够酷</p>
         <p><span>免费注册</span></p>
-        <img  class="index-box1-img " src="../image/index-bg1.png" alt="">
+        <div class="index-box1-div">
+          <img  class="index-box1-img " src="../image/index-bg1.png" alt="">
+        </div>
+        
         </div>
     </div>
     <div class="index-box2">
@@ -20,21 +23,21 @@
       <div class="box2-list">
         <div>
           <p><img src="../icon/box2-list-icon1.png" alt=""></p>
-          <p><h3>专业建站工具</h3></p>
+          <p><span>专业建站工具</span></p>
           <p class="box2-list-word">
          专业建站工具是面向具有一定Html、css基础的专业人员，可使用专业建站工具进行网站前端设计，每一个细节都可灵活掌控，并可导出符合标准的代码进行二次开发。
          </p>
         </div>
         <div>
           <p><img src="../icon/box2-list-icon2.png" alt=""></p>
-          <p><h3>专业建站工具</h3></p>
+          <p><span>专业建站工具</span></p>
           <p class="box2-list-word">
          专业建站工具是面向具有一定Html、css基础的专业人员，可使用专业建站工具进行网站前端设计，每一个细节都可灵活掌控，并可导出符合标准的代码进行二次开发。
          </p>
         </div>
         <div>
           <p><img src="../icon/box2-list-icon3.png" alt=""></p>
-          <p><h3>专业建站工具</h3></p>
+          <p><span>专业建站工具</span></p>
           <p class="box2-list-word">
          专业建站工具是面向具有一定Html、css基础的专业人员，可使用专业建站工具进行网站前端设计，每一个细节都可灵活掌控，并可导出符合标准的代码进行二次开发。
          </p>
@@ -47,17 +50,216 @@
             <p class="list-msg3">...</p>
             <p class="list-msg4">Coolsite360模块建站工具，是国内首创的结构化网页设计工具，目前已提供数十款预制模块，悬浮导航、轮播、相册、价格表、内容列表、时间轴、地图...，通过预制模块，可快速搭建网站。</p>
         </div>
-        <div class="box2-list-img">
+        
+      </div>
+      <div class="box2-list-img">
           <img class="box2-list-imgshow" src="../image/box2-list-img.png" alt="">
         </div>
-      </div>
-      
 
     </div>
-    <div class="index-box3">
-dsadas
+
+
+
+    <div class="index-box2 index-box3">
+      <div class="box2-list">
+            <div class="box2-list-msg">
+            <p class="list-msg1">PROFESSIONAL</p>
+            <p class="list-msg2">
+                全自由度专业建站工具</p>
+            <p class="list-msg3">...</p>
+            <p class="list-msg4">Coolsite360专业建站工具，是国内唯一一款支持全自由度的可视化网页设计工具，支持flex布局、自定义Class、自定义属性、支持嵌入自定义代码，从网页布局到元素的样式设定，从动画到交互，CoolSite360为您提供全自由度的设计掌控。并可导出符合标准的代码进行二次开发。</p>
+            <img class="box2-list-imgshow" src="../image/box3-list-img.png" alt="">
+        </div>
+        </div>
       </div>
+
+
+
+
+
+  <div class=" index-box2 index-box4">
+    <div class="box2-list">
+            <div class="box2-list-msg">
+            <p class="list-msg1">DOC EDITOR</p>
+            <p class="list-msg2">
+                可视化Markdown文档编辑器</p>
+            <p class="list-msg3">...</p>
+            <p class="list-msg4">Coolsite360 文档编辑器，支持可视化以及Markdown双模式编辑，除了可以用来编写单篇文档外，还可方便的通过文档集功能，进行多篇文档的整合，适用于以下场景。</p>
+            <p>发布文章，博客</p>
+            <p>撰写发布技术文稿（代码支持）</p>
+            <p>撰写发布学术论文（LaTeX 公式支持）</p>
+            <p>编写产品手册及帮助文档（支持文档集组织）</p>
+        </div>
+
+        <div class="index-box4-bottom">
+          <div>
+            <div class="box4-icon box4-icon1"><i></i></div>
+            <div class="box4-word">
+              <h3>可视化、Markdown双模式编辑</h3>
+                <p>编辑器支持可视化以及Markdown双模式编辑，既保证了文档的标准化，又降低了对编写者的要求，提升编写体验。</p>
+            </div>
+          </div>
+          <div>
+            <div class="box4-icon box4-icon2"><i></i></div>
+            <div class="box4-word">
+              <h3>支持多篇文档快速组织</h3>
+                <p>通过文档集功能，进行多篇文档的整合，并可快速嵌入网站，可用来编写产品手册及帮助文档</p>
+            </div>
+          </div>
+          <div>
+            <div class="box4-icon box4-icon3"><i></i></div>
+            <div class="box4-word">
+              <h3>支持全文检索</h3>
+                <p>文档集支持全文检索，用户可快速在多篇文档中查找所需内容。</p>
+            </div>
+          </div>
+          </div> 
+
+          <div class="box4-btn">
+            <a href="">查看简介文档</a>
+          </div>
+        </div>
   </div>
+  
+  
+    <div class=" index-box2 index-box4 index-box5">
+    <div class="box2-list">
+            <div class="box2-list-msg">
+            <p class="list-msg1">CONTENT MANAGEMENT</p>
+            <p class="list-msg1">SYSTEM</p>
+            <p class="list-msg2">
+                内容管理系统（CMS2.0）</p>
+            <p class="list-msg3">...</p>
+            <p class="list-msg4">Coolsite360内置强大的内容管理系统，除了预制的文章、商品展示、项目案例内容类型，还提供强大的自定义功能，可添加自定义字段，满足不同的内容展示需求。</p>
+            <!-- <p>发布文章，博客</p>
+            <p>撰写发布技术文稿（代码支持）</p>
+            <p>撰写发布学术论文（LaTeX 公式支持）</p>
+            <p>编写产品手册及帮助文档（支持文档集组织）</p> -->
+        </div>
+
+        <div class="index-box4-bottom">
+          <div>
+            <div class="box4-icon box4-icon4"><i></i></div>
+            <div class="box4-word">
+              <h3>自定义内容字段</h3>
+              <h3>Custom field</h3>
+                <p>可自定义内容类型，可添加字符串、数字、图片、图片集、布尔、链接、属性列表等自定义字段。</p>
+            </div>
+          </div>
+          <div>
+            <div class="box4-icon box4-icon5"><i></i></div>
+            <div class="box4-word">
+              <h3>自定义筛选过滤及排序</h3>
+              <h3>Filter and sort</h3>
+                <p>支持分类、tag、关键词过滤，可通过自定义过滤表进行支持组合过滤和排序。</p>
+            </div>
+          </div>
+          <div>
+            <div class="box4-icon box4-icon3"><i></i></div>
+            <div class="box4-word">
+              <h3>支持关键词检索</h3>
+              <h3>Keywords</h3>
+                <p>通过搜索框组件，可实现关键词搜索。</p>
+            </div>
+          </div>
+          </div> 
+
+          <div class="box4-btn">
+            <a href="">查看 CMS（2.0）demo网站</a>
+          </div>
+        </div>
+  </div>
+
+  <div class="index-box2 index-box4 index-box6">
+  
+  <div class="box2-list">
+            <div class="box2-list-msg">
+            <p class="list-msg1">FEATURE</p>
+            <p class="list-msg2">
+                特色系统</p>
+            <p class="list-msg3">...</p>
+        </div>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">响应式</p>
+        <p class="word-b">Responsive </p>
+        <p>支持网格系统及Flex布局，可轻松设计自动适配于桌面及移动端的响应式网站。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">网络字体</p>
+        <p class="word-b">Web fonts</p>
+        <p>支持网络字体，目前已集成思源黑体及部分google fonts，专业版编辑器可灵活设置所需的网络字体。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">SVG矢量图形</p>
+        <p class="word-b">Vectors</p>
+        <p>支持SVG矢量图形，支持SVG代码的粘贴，并可设置SVG路径动画及变形动画。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">CSS样式粘贴</p>
+        <p class="word-b">Paste CSS</p>
+        <p>支持自定义class，并可通过PS、Sketch复制粘贴CSS代码，可快速进行样式设置。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">表单</p>
+        <p class="word-b">Forms</p>
+        <p>可设计表单进行数据收集，专业版编辑器可方便地进行自定义输入框样式设计及数据绑定。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">CMS内容管理</p>
+        <p class="word-b">CMS</p>
+        <p>通过CMS内容管理功能，可设计自定义的动态内容网格列表及内容模板，通过数据绑定功能，实现动态内容的展现。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">动画</p>
+        <p class="word-b"> Animation</p>
+        <p>丰富的动画、动效属性，让网站不再静默，现在，轻松点下鼠标，让网站活跃起来。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">触发器交互</p>
+        <p class="word-b">Interaction</p>
+        <p>支持触发器交互，无需编程便可设计具有丰富交互效果的酷站，满足您的创意需求。</p>
+        </li>
+        <li>
+          <div class="index-box6-icon">
+            <i></i>
+            </div>
+        <p class="word-b">导出代码</p>
+        <p class="word-b"> Code export</p>
+        <p>可输出符合标准的html文档结构及css样式表，便于后期添加个性化功能。</p>
+        </li>
+        </div>
+  </div>
+  
+  
+  
+  
+  </div>
+
+
 </template>
 
 <script>
@@ -102,12 +304,13 @@ export default {
       $(window).scroll(function(e) {
         w = $(this).scrollTop();
         // console.log(w)
-        if (r <= w && r >= 1500) {
+        if (r <= w && r >= 1200) {
           //滚轴到520px时开启动画
           console.log("下" + r);
           $(".box2-list-img img").addClass("box2-list-imgshow");
+          // alert("aaa")
         } else {
-          // $(".box2-list-img img").removeClass("box2-list-imgshow");
+          $(".box2-list-img img").removeClass("box2-list-imgshow");
         }
         setTimeout(function() {
           r = w;
@@ -124,7 +327,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="less" scoped>
+<style lang="less">
 .index-box1 {
   border-bottom: 1px solid #585858;
   height: 952px;
@@ -144,7 +347,7 @@ export default {
     opacity: 0.5;
   }
   .box1-word {
-    padding: 0 7%;
+    padding: 0 20%;
     padding-top: 80px;
     position: absolute;
     top: 0;
@@ -177,20 +380,23 @@ export default {
         color: #fff;
       }
     }
-    img {
-      display: block;
-      opacity: 0;
-      width: 100%;
-      height: 532px;
-      margin-top: 90px;
-      box-shadow: rgba(0, 0, 0, 0.227451) 0px 0px 21px 0px;
-    }
-    .index-box1-imgshow {
-      display: block;
-      margin-top: 64px;
-      transition: all 0.4s linear;
-      -webkit-transition: all 0.4s linear;
-      opacity: 1;
+    .index-box1-div {
+      max-width: 1200px;
+      img {
+        display: block;
+        opacity: 0;
+        width: 100%;
+        height: 532px;
+        margin-top: 90px;
+        box-shadow: rgba(0, 0, 0, 0.227451) 0px 0px 21px 0px;
+      }
+      .index-box1-imgshow {
+        display: block;
+        margin-top: 64px;
+        transition: all 0.4s linear;
+        -webkit-transition: all 0.4s linear;
+        opacity: 1;
+      }
     }
   }
 }
@@ -222,9 +428,12 @@ export default {
     -webkit-transition: all 1s linear;
   }
   .box2-list {
+    max-width: 1200px;
     overflow: hidden;
-    padding: 0 3.6%;
-    padding-bottom:120px;
+    // padding: 0 3.6%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-bottom: 120px;
     div:first-child {
       background-color: #000;
     }
@@ -264,53 +473,208 @@ export default {
       }
     }
   }
-  .box2-list-msg{
-    width:100%!important;
-    height: auto!important;
-    margin-bottom:40px;
+  .box2-list-msg {
+    width: 100% !important;
+    height: auto !important;
+    margin-bottom: 40px;
   }
-  .list-msg1{
-        // line-height: 1.3em;
+  .list-msg1 {
+    // line-height: 1.3em;
     margin-top: 68px;
     font-family: Roboto;
     // margin-bottom: 25px;
     font-weight: lighter;
     font-size: 60px;
-    color: rgb(74, 134, 232); 
+    color: rgb(74, 134, 232);
   }
-  .list-msg2{
+  .list-msg2 {
     font-size: 22px;
-    margin-bottom:18px;
+    // margin-bottom: 18px;
   }
-  .list-msg3{
+  .list-msg3 {
     font-size: 24px;
-    margin-bottom:31px;
+    // margin-bottom: 31px;
+    margin-bottom: 14px;
   }
-  .list-msg4{
+  .list-msg4 {
     font-size: 14px;
     opacity: 0.8;
-    width:55%;
-    margin:0 auto;
+    width: 55%;
+    margin: 0 auto;
   }
-  .box2-list-img{
-    width:100%!important;
-    img{
+  .box2-list-img {
+    width: 100% !important;
+    img {
       display: block;
-      width:100%;
+      width: 100%;
       opacity: 0;
-      -wwbkit-transition:all .5s linear;
-      transition:all .5s linear;
+      -wwbkit-transition: all 0.5s linear;
+      transition: all 0.5s linear;
       // height:100%;
-      
     }
-    .box2-list-imgshow{
+    .box2-list-imgshow {
       opacity: 1;
+      transition: all 1s linear;
+      -webkit-transition: all 1s linear;
     }
   }
 }
-.index-box3{
-  width:100%;
-  height:500px;
+.index-box3 {
+  .box2-list {
+    padding-bottom: 0;
+  }
+  width: 100% !important;
   background-color: #000;
+  .box2-list-msg {
+    margin-bottom: 0;
+  }
+  .box2-list-imgshow {
+    margin-top: 50px;
+    max-height: 684px;
+  }
+}
+.index-box4 {
+  .box2-list div:first-child {
+    background-color: rgb(67, 67, 67);
+  }
+  background-color: rgb(67, 67, 67);
+  .list-msg4 {
+    padding-bottom: 30px;
+  }
+  .box2-list div:nth-child(2) {
+    background-color: #434343;
+  }
+  .box2-list div:nth-child(3) {
+    height: auto;
+    margin: 0 auto;
+    background-color: #434343;
+    a {
+      display: block;
+      width: 300px;
+      height: 50px;
+      line-height: 50px;
+      background-color: rgb(74, 134, 232);
+      border-radius: 6px;
+      color: rgb(222, 222, 222);
+      margin: 0 auto;
+    }
+    // :hover{
+    //   background-color: #428bca;
+    //   border-color: #357ebd;
+    //   color:#fff;
+    //   }
+  }
+}
+.index-box4-bottom {
+  width: 100% !important;
+  div {
+    padding: 11px;
+    box-sizing: border-box;
+    .box4-icon {
+      text-align: center;
+      width: 100%;
+      height: auto;
+
+      i {
+        display: block;
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+        border: 1px solid #5ee2ff;
+        margin: 0 auto;
+      }
+    }
+    .box4-icon1 i {
+      background: url(../icon/index-box4-icon1.png)center center no-repeat;
+      background-size: 35%;
+    }
+    .box4-icon2 i {
+      background: url(../icon/index-box4-icon2.png)center center no-repeat;
+      background-size: 35%;
+    }
+    .box4-icon3 i {
+      background: url(../icon/index-box4-icon3.png)center center no-repeat;
+      background-size: 35%;
+    }
+    .box4-word {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+.box4-btn {
+  width: 100% !important;
+}
+.index-box5 {
+  .box2-list div:first-child {
+    background-color: #000;
+  }
+  background-color: #000;
+  .list-msg4 {
+    padding-bottom: 30px;
+  }
+  .box2-list div:nth-child(2) {
+    background-color: #000;
+  }
+  .box2-list div:nth-child(3) {
+    background-color: #000;
+  }
+  background-color: #000 !important;
+  .box2-list-msg {
+    background-color: #000 !important;
+  }
+  .index-box4-bottom {
+    background-color: #000 !important;
+    .box4-icon4 i {
+      background: url(../icon/index-box4-icon4.png)center center no-repeat;
+      background-size: 35%;
+    }
+    .box4-icon5 i {
+      background: url(../icon/index-box4-icon5.png)center center no-repeat;
+      background-size: 35%;
+    }
+  }
+  .box4-btn {
+    background-color: #000 !important;
+  }
+}
+.index-box6 {
+  .box2-list {
+    overflow: hidden;
+    li {
+     box-sizing: border-box;
+      width: 30%;
+      padding: 1%;
+      list-style: none;
+      float: left;
+      text-align: center;
+      min-height: 250px;
+      max-height: 263px;
+      margin-bottom:30px;
+      .word-b{
+             font-weight: bolder;
+             font-size: 18px;
+      }
+      .index-box6-icon {
+        width: 100%;
+        height: auto;
+        padding-bottom: 10px;
+        margin: 0 auto;
+        i {
+          display: block;
+          margin: 0 auto;
+          width: 100px;
+          height: 100px;
+          background: url(../icon/index-icon1.png)center center no-repeat;
+          background-size: 100%;
+        }
+      }
+      p {
+        box-sizing: border-box;
+        padding: 10px;
+        font-size: 13px;
+      }
+    }
+  }
 }
 </style>
